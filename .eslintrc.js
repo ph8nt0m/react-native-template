946ba4ce3faca3dde27845d9d84a8539392460b1
ph8nt0m <phant0m@kakao.com>
2022-04-18 20:51:09 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
